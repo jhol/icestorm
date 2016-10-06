1fc508b878e83b843784a159d6535953c0d1e32c
Joel Holdsworth <joel@airwebreathe.org.uk>
2016-10-05 21:31:04 -0600

    WIP

--- icebox/vtr.py
+++ icebox/vtr.py
@@ -18,46 +18,44 @@
 import lxml.etree as et
 import sys
 
-class Model:
-  def __init__(self, name, inputs, outputs, clocks=[]):
-    self.name = name
-    self.inputs = inputs
-    self.outputs = outputs
-    self.clocks = clocks
+e_architecture = et.Element('architecture')
 
-  def model(self):
-    def create_ports_node(type):
-      e_ports = et.Element(type + '_ports')
-      e_ports.extend([et.Element('port', name=i) for i in
-          getattr(self, type + 's', [])])
-      return e_ports
-    e_model = et.Element('model', name='.gate ' + self.name)
-    e_model.extend([create_ports_node(t) for t in ['input', 'output', 'clock']])
-    return e_model
+#
+# Models
+#
+
+class Model:
+    def __init__(self, name, inputs, outputs, clocks=[]):
+        self.name = name
+        self.inputs = inputs
+        self.outputs = outputs
+        self.clocks = clocks
+
+    def model(self):
+        def create_ports_node(type):
+            e_ports = et.Element(type + '_ports')
+            e_ports.extend([et.Element('port', name=i) for i in
+                getattr(self, type + 's', [])])
+            return e_ports
+        e_model = et.Element('model', name='.gate ' + self.name)
+        e_model.extend(
+            [create_ports_node(t) for t in ['input', 'output', 'clock']])
+        return e_model
 
 def dff_types():
-  return ([(neg_clk, en, False)
-    for en in [False, True] for neg_clk in [False, True]] +
-    [(neg_clk, en, True, set) for set in [False, True]
-    for en in [False, True] for neg_clk in [False, True]])
+    return ([(neg_clk, en, False)
+        for en in [False, True] for neg_clk in [False, True]] +
+        [(neg_clk, en, True, set) for set in [False, True]
+        for en in [False, True] for neg_clk in [False, True]])
 
 def dff_suffix(neg_clk, en, sync, set=False):
-  return (('N' if neg_clk else '') + ('E' if en else '') +
-    ('S' + ('S' if set else 'R') if sync else ''))
+    return (('N' if neg_clk else '') + ('E' if en else '') +
+        ('S' + ('S' if set else 'R') if sync else ''))
 
 def dff(neg_clk, en, sync, set=False):
-  return Model('SB_DFF' + dff_suffix(neg_clk, en, sync, set),
-    ['D'] + (['E'] if en else []) + ((['S'] if set else ['R']) if sync else []),
-    ['Q'], ['C'])
-
-def plb_mode(neg_clk, en, sync, set=False):
-  e_mode = et.Element('mode', name='PLB' + dff_suffix(neg_clk, en, sync, set))
-  return e_mode
-
-def pb_type_plb():
-  e_pb_type = et.Element('pb_type', name='PLB')
-  e_pb_type.extend([plb_mode(*t) for t in dff_types()])
-  return e_pb_type
+    return Model('SB_DFF' + dff_suffix(neg_clk, en, sync, set),
+        ['D'] + (['E'] if en else []) + ((['S'] if set else ['R']) if sync else []),
+        ['Q'], ['C'])
 
 carry = Model('SB_CARRY', ['CI', 'I0', 'I1'], ['C0'])
 dffs = [dff(*t) for t in dff_types()]
@@ -66,30 +64,185 @@ lut4 = Model('SB_LUT4', ['I0', 'I1', 'I2', 'I3'], ['O'])
 
 models = [carry] + dffs + [gb, lut4]
 
-e_architecture = et.Element('architecture')
-
 e_models = et.Element('models')
 e_models.extend([m.model() for m in models])
 e_architecture.append(e_models)
 
+#
+# Layout
+#
+
 e_architecture.append(et.Element('layout', width='33', height='33'))
 
+#
+# Device
+#
+
 e_device = et.Element('device')
+
+# Taken from vpr/sample_arch.xml which has realistic model data for a
+# hypothetical 40nm architecure
 e_device.append(et.Element(
     'sizing', R_minW_nmos='8926', R_minW_pmos='16067'))
 e_device.append(et.Element(
     'timing', C_ipin_cblock='1.47e-15', T_ipin_cblock='7.247000e-11'))
+
 e_device.append(et.Element('switch_block'))
 e_architecture.append(e_device)
 
-e_switchlist = et.Element('switchlist')
-e_architecture.append(e_switchlist)
+#
+# Segment List
+#
 
 e_segmentlist = et.Element('segmentlist')
 e_architecture.append(e_segmentlist)
 
+for length in [4, 12]:
+    e_segment = et.Element('segment', name='span%d' % length, freq='50',
+        length=str(length), type='bidir', Cmetal='0')
+
+    e_sb = et.Element('sb', type='pattern')
+    e_sb.text = ' '.join(['0'] * (length + 1))
+    e_segment.append(e_sb)
+
+    e_cb = et.Element('cb', type='pattern')
+    e_cb.text = ' '.join(['1'] * length)
+    e_segment.append(e_cb)
+
+    e_wire_switch = et.Element('wire_switch', name='?')
+    e_segment.append(e_wire_switch)
+
+    e_opin_switch = et.Element('opin_switch', name='?')
+    e_segment.append(e_opin_switch)
+
+    e_segmentlist.append(e_segment)
+
+#
+# Switch List
+#
+
+e_switchlist = et.Element('switchlist')
+e_architecture.append(e_switchlist)
+
+#
+# Complex Block List
+#
+
+def ios(tag, name_fmt, indices=1, num_pins=1, **kw):
+  return [et.Element(tag, name=name_fmt.format(i), num_pins=str(num_pins),
+    equivalent='false', **kw) for i in range(indices)]
+
 e_complexblocklist = et.Element('complexblocklist')
-e_complexblocklist.append(pb_type_plb())
 e_architecture.append(e_complexblocklist)
+
+#
+# LOGIC
+#
+
+def plb_mode(neg_clk, en, sync, set=False):
+    e_mode = et.Element('mode', name='PLB' + dff_suffix(neg_clk, en, sync, set))
+
+    e_pb_type = et.Element('pb_type', num_pb='8')
+
+    e_pb_type_carry = et.Element('pb_type', name='CARRY', blif='.gate SB_CARRY')
+    e_pb_type_carry.extend(ios('input', 'CI'))
+    e_pb_type_carry.extend(ios('input', 'I{0}', 2))
+    e_pb_type_carry.extend(ios('output', 'CO'))
+    e_pb_type.append(e_pb_type_carry)
+
+    e_pb_type_lut4 = et.Element('pb_type', name='LUT4', blif='.gate SB_LUT4')
+    e_pb_type_lut4.extend(ios('input', 'I', 1, 4))
+    e_pb_type_lut4.extend(ios('output', 'O'))
+    e_pb_type.append(e_pb_type_lut4)
+
+    e_mode.append(e_pb_type)
+
+    return e_mode
+
+def pb_type_plb():
+    e_pb_type = et.Element('pb_type', name='PLB', height='2')
+
+    # Declare I/O
+    e_pb_type.extend(ios('clock', 'lutff_global/clk'))
+    e_pb_type.extend(ios('input', 'lutff_global/cen'))
+    e_pb_type.extend(ios('input', 'lutff_global/s_r'))
+    e_pb_type.extend(ios('input', 'carry_in'))
+    e_pb_type.extend(ios('input', 'carry_in_mux'))
+    e_pb_type.extend(ios('input', 'lutff_{0}/in', 8, 4))
+    e_pb_type.extend(ios('output', 'lutff_{0}/lout', 7))
+    e_pb_type.extend(ios('output', 'lutff_{0}/out', 8))
+    e_pb_type.extend(ios('output', 'lutff_{0}/cout', 8))
+
+    # Modes
+    e_pb_type.extend([plb_mode(*t) for t in dff_types()])
+
+    # Declare locations
+    e_gridlocations = et.Element('gridlocations')
+    e_gridlocations.append(et.Element('loc', type='fill', priority='0'))
+    e_pb_type.append(e_gridlocations)
+
+    return e_pb_type
+
+e_complexblocklist.append(pb_type_plb())
+
+
+#
+# RAM
+#
+
+def pb_type_ram():
+    e_pb_type = et.Element('pb_type', name='RAM')
+    #setattr(e_pb_type, 'class', 'memory')
+
+    # Declare I/O
+    e_pb_type.extend(ios('output', 'ram/RDATA', 1, 16, port_class='data_out1'))
+    e_pb_type.extend(ios('input', 'ram/RADDR', 1, 11, port_class='address1'))
+    e_pb_type.extend(ios('input', 'ram/WADDR', 1, 11, port_class='address2'))
+    e_pb_type.extend(ios('input', 'ram/MASK', 1, 16))
+    e_pb_type.extend(ios('input', 'ram/WDATA', 1, 16, port_class='data_in2'))
+    e_pb_type.extend(ios('input', 'ram/RCLKE'))
+    e_pb_type.extend(ios('clock', 'ram/RCLK'))
+    e_pb_type.extend(ios('input', 'ram/RE'))
+    e_pb_type.extend(ios('input', 'ram/WCLKE'))
+    e_pb_type.extend(ios('clock', 'ram/WCLK'))
+    e_pb_type.extend(ios('input', 'ram/WE', port_class='write_en2'))
+
+    # Declare locations
+    e_gridlocations = et.Element('gridlocations')
+    for col in [8, 25]:
+        e_gridlocations.append(et.Element('loc', type='col', start=str(col),
+            repeat='1', priority='1'))
+    e_pb_type.append(e_gridlocations)
+
+    return e_pb_type
+
+e_complexblocklist.append(pb_type_ram())
+
+
+#
+# IO
+#
+
+def pb_type_io():
+    e_pb_type = et.Element('pb_type', name='IO')
+
+    # Declare I/O
+    e_pb_type.extend(ios('input', 'io_{0}/D_IN', 2, 2))
+    e_pb_type.extend(ios('output', 'io_{0}/D_OUT', 2, 2))
+    e_pb_type.extend(ios('input', 'io_{0}/OUT_ENB', 2))
+    e_pb_type.extend(ios('input', 'io_global/cen'))
+    e_pb_type.extend(ios('input', 'io_global/inclk'))
+    e_pb_type.extend(ios('output', 'io_global/outclk'))
+    e_pb_type.extend(ios('input', 'io_global/latch'))
+
+    # Declare locations
+    e_gridlocations = et.Element('gridlocations')
+    e_gridlocations.append(et.Element('loc', type='perimeter', priority='2'))
+    e_pb_type.append(e_gridlocations)
+
+    return e_pb_type
+
+e_complexblocklist.append(pb_type_io())
+
 
 sys.stdout.write(et.tostring(e_architecture, pretty_print=True).decode('utf-8'))
